f2c3463a984ca67739527c0c41675a20b3795229
noraalobaidi <nalobaidi_@hotmail.com>
2022-06-07 19:55:18 +0300

    Modify app.js

--- app.js
+++ app.js
@@ -9,9 +9,27 @@ const cors = require("cors");
 const app = express();
 connectDb();
 app.use(express.json());
+app.use(passport.initialize());
+passport.use(localStrategy);
+passport.use(jwtStrategy);
 app.use(cors());
 
 app.use("/categories", categoriesRoutes);
+app.use(userRoutes);
+app.use((req, res, next) => {
+  const err = new Error("Not Found");
+  err.status = 404;
+  next(err);
+});
+
+app.use((err, req, res, next) => {
+  res.status(err.status || 500);
+  res.json({
+    error: {
+      message: err.message || "Internal Server Error",
+    },
+  });
+});
 // app.use((req, res, next) => {
 //   const err = new Error("Not Found");
 //   err.status = 404;
