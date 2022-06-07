8471a3e9b27ff3e0ada4fd1c30d586669b2dbe00
noraalobaidi <nalobaidi_@hotmail.com>
2022-06-07 18:35:18 +0300

    Add Ingredient modal

---
+++ models/Ingredient.js
@@ -0,0 +1,12 @@
+const { model, Schema } = require("mongoose");
+
+const IngredientSchema = new Schema({
+  name: {
+    type: String,
+    unique: true,
+  },
+  image: String,
+  recipes: [{ type: Schema.Types.ObjectId, ref: "Recipe" }],
+});
+
+module.exports = model("Ingredient", IngredientSchema);
