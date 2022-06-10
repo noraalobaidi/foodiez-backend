a5d41f19ba69eec3ce2bcf864d39fa35a541f2f8
dohaisalive <dohaisalive@gmail.com>
2022-06-10 17:05:48 +0300

    modify recipe model

--- api/ingredients/ingredients.controllers.js
+++ api/ingredients/ingredients.controllers.js
@@ -1,22 +1,19 @@
 const Ingredient = require("../../models/Ingredient");
 
-
 exports.fetchIngredients = async (req, res) => {
   try {
-    const ingredients=await Ingredient.find();
+    const ingredients = await Ingredient.find();
     res.json(ingredients);
   } catch (err) {
     res.status(500).json("Server Error");
   }
 };
 
-exports.createIngredient = async(req,res)=>{
-try{
-  const newIngredient = await Ingredient.create(req.body);
+exports.createIngredient = async (req, res) => {
+  try {
+    const newIngredient = await Ingredient.create(req.body);
     res.status(201).json(newIngredient);
-
-}
-catch(error){
-  res.status(500).json("Server Error");
-}
+  } catch (error) {
+    res.status(500).json("Server Error");
+  }
 };
--- models/Recipe.js
+++ models/Recipe.js
@@ -4,13 +4,15 @@ const RecipeSchema = new Schema({
   name: String,
   image: String,
   category: String,
+  slug: String,
+  steps: String,
   ingredients: [
     {
       type: Schema.Types.ObjectId,
       ref: "Ingredient",
     },
   ],
-  user : {
+  user: {
     type: Schema.Types.ObjectId,
     ref: "User",
   },
